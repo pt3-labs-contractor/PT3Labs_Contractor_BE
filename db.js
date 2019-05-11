b397bbca3e41bd4c48701352b14e02f3feeb7da0
edreeseg <edward.reeseg@gmail.com>
2019-05-11 13:44:52 -0400

    Pushing to origin

--- db.js
+++ db.js
@@ -1,29 +1,33 @@
 const { Client } = require('pg');
 
 module.exports = {
-    connect,
-    disconnect,
-    select,
-}
+  connect,
+  disconnect,
+  query,
+};
 
 const client = new Client();
 
-async function connect(){
-    try {
-        await client.connect();
-    } catch(err) {
-        console.log(err);
-    }
+async function connect() {
+  try {
+    await client.connect();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-async function disconnect(){
-    try {
-        await client.end();
-    } catch(err) {
-        console.log(err);
-    }
+async function disconnect() {
+  try {
+    await client.end();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-async function query(str){
-    return await client.query(str);
+async function query(str) {
+  try {
+    return client.query(str);
+  } catch (err) {
+    console.log(err);
+  }
 }
--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 const { connect, disconnect, query } = require('./db');
 
-handleDBConnect();
+connect();
+
+query('SELECT * FROM contractors;')
+  .then(res => console.log(res.rows))
+  .catch(err => console.log('Error: ', err));
 
 const port = process.env.PORT || 5000;
 
