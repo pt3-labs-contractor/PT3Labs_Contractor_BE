8efdf4e320c9afd692c3db257afb9444f4a91a0a
edreeseg <edward.reeseg@gmail.com>
2019-05-13 17:27:07 -0400

    Adding json body parser

--- api/server.js
+++ api/server.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(helmet());
+app.use(express.json());
 app.use('/contractors', contractorRoutes);
 app.use('/users', userRoutes);
 app.use('/schedules', schedulesRoutes);
