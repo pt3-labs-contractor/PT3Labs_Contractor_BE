5dd0732fc869127e9b4d49e105c9069f6d7568d5
Bao Pham <ryanvuepham@gmail.com>
2019-05-13 01:16:14 -0500

    login with google is partially working

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PT3Labs_Contractor_BE
+
 Back End
+
+## Passport with Google_Auth_2.0
+
+In order to use Google as authentication, we need a clientID and clientSecret. visit https://console.developers.google.com
+and create a new project, then click on "Credentials" > OAuth client ID > Web application: name it whatever you want >
+
+client id: 887826498881-qpoqqrnaa338vqgkfvs21arpj90peqfp.apps.googleusercontent.com
+client secret: 1B8pZs0dVExwPpV_S0Ty0pJV
---
+++ server.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const server = express()
+const passport = require('passport')
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy // .Strategy??
+const HaveACookie = require('express-session')
+
+// give me a cookie baby
+
+const session = require('express-session')
+
+// configure express-session middleware
+server.use(
+	HaveACookie({
+		name: 'notsession', // default is connect.sid
+		secret: 'nobody tosses a dwarf!',
+		cookie: {
+			maxAge: 1 * 24 * 60 * 60 * 1000,
+			secure: true // only set cookies over https. Server will not send back a cookie over http.
+		}, // 1 day in milliseconds
+		httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+		resave: false,
+		saveUninitialized: false
+	})
+)
+
+server.use(passport.initialize()) // initialize passport
+//server.use(passport.session()) // persist login sessions
+// Configure view engine to render EJS templates.
+// Configure view engine to render EJS templates.
+server.set('views', __dirname + '/views')
+server.set('view engine', 'ejs')
+
+// Google Strategy
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID:
+				'887826498881-qpoqqrnaa338vqgkfvs21arpj90peqfp.apps.googleusercontent.com', // your client id here
+			clientSecret: '1B8pZs0dVExwPpV_S0Ty0pJV', // your client secret here
+			callbackURL: 'http://localhost:5000/auth/google/callback'
+		},
+		(accessToken, refreshToken, profile, done) => {
+			done(null, profile) // passes the profile data to serializeUser
+		}
+	)
+)
+
+// Used to stuff a piece of information into a cookie
+passport.serializeUser((user, done) => {
+	done(null, user)
+})
+
+// Used to decode the received cookie and persist session
+passport.deserializeUser((user, done) => {
+	done(null, user)
+})
+
+// Middleware to check if the user is authenticated
+function isUserAuthenticated(req, res, next) {
+	if (req.user) {
+		next()
+	} else {
+		res.send('not authorized, please log in now!')
+	}
+}
+
+// end points
+server.get('/', (req, res) => {
+	res.render('index.ejs')
+})
+
+// passport.authenticate(middleware), specifying 'google' strategy for the requests
+server.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile'] // Used to specify the required data
+	})
+)
+
+// The middleware receives the data from Google and runs the function on Strategy config
+server.get(
+	'/auth/google/callback',
+	passport.authenticate('google'),
+	(req, res) => {
+		res.redirect('/secret')
+	}
+)
+
+// Secret endpoint
+server.get('/secret', isUserAuthenticated, (req, res) => {
+	res.send('You have reached the secret route')
+})
+
+// Logout endpoint
+server.get('/logout', (req, res) => {
+	req.logout()
+	res.redirect('/')
+})
+
+server.listen(5000, () => {
+	console.log('Server is live on port 5000!')
+})
