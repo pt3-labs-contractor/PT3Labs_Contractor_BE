97f1bfbe948f07d8d4365f26b160612445edfa32
edreeseg <edward.reeseg@gmail.com>
2019-05-13 22:37:55 -0400

    Altering user route to display user phone number

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   const users = await query(
-    'SELECT id, username, email, contractor_id, created_at FROM users;'
+    'SELECT id, username, phone_number, email, contractor_id, created_at FROM users;'
   );
   res.json({ users: users.rows });
 });
