16f7293593b83dec0c2ef6d6e172fec260db1b1b
edreeseg <edward.reeseg@gmail.com>
2019-05-13 22:23:55 -0400

    Added sessions, tomorrow will examine the exact flow of the login/when additional user information is obtained

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session');
+const passport = require('passport');
 const {
   contractorRoutes,
   userRoutes,
@@ -12,6 +14,21 @@ require('../config/passport-setup');
 
 const app = express();
 
+app.use(
+  session({
+    name: process.env.SESSION_NAME,
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false, // When to get consent?  Give consent as part of Google consent screen?
+    httpOnly: true,
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      secure: false, // https only, ideally will be true in production
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
