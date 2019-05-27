95cab59e96fc82258d1e8356735b7249b906a756
Bao Pham <ryanvuepham@gmail.com>
2019-05-26 20:05:40 -0500

    user endpoints finished

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -12,22 +12,63 @@ router.get('/', async (req, res) => {
   res.json({ users: users.rows });
 });
 
-// Get user by id
 router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const user = await query(`SELECT FROM * users WHERE id=$1`, [id]);
-  res.json({ users: user.row[0] });
+  const user = await query('SELECT * FROM users where id= $1', [id]);
+  res.json({ users: users.rows });
 });
 
-// router.put('/:id', (req, res) => {
-//   const {id} = req.params
-//   const {name, email, }
-// })
+router.post('/', async (req, res) => {
+  try {
+    const user = await query(
+      'INSERT INTO users (google_id, username, phone_number, email, contractor_id) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [
+        req.body.google_id,
+        req.body.username,
+        req.body.phone_number,
+        req.body.email,
+        req.body_contractor_id,
+      ]
+    );
+    return res.json(user.rows[0]);
+  } catch (err) {
+    return err;
+  }
+});
 
-router.delete('/', async (req, res) => {
-  const { id } = req.params; // req.decoded
-  const user = await query(`DELETE FROM users WHERE id=$1`, [id]);
-  res.json({ users: user.row[0] });
+router.update('/:id', async (req, res) => {
+  try {
+    const user = await query(
+      'UPDATE users SET phone_number = ($1), email = ($2) WHERE id = ($3) RETURNING *',
+      [req.body.phone_number, req.body.email, req.params.id]
+    );
+    return res.json(user.rows[0]);
+  } catch (err) {
+    return err;
+  }
+});
+
+// Asyn/await. use this endpoint if the one below isnt working
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const user = await query('DELETE FROM users WHERE id = $1', [
+//       req.params.id,
+//     ]);
+//     return res.json({ message: `${user} deleted` });
+//   } catch (err) {
+//     return err;
+//   }
+// });
+
+// As a callback
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  query('DELETE FROM users WHERE id = $1', [id], (err, res) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).send(`user deleted with id: ${id}`);
+  });
 });
 
 module.exports = router;
