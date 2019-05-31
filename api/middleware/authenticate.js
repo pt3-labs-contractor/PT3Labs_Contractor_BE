c6f8b264947666f2883a9cdd954bdc1ca44a1c40
echopecho <pbsmithtbn@gmail.com>
2019-05-30 22:54:53 -0500

    Update authenticate middleware
    I was looking through the code and saw that the authenticate wasn't actually returning the decoded token. This new code, according to the docs is the best way to return the decoded token while also verifying the token.  I noticed some weird behavior when trying to make a put request to /users. Take a look at it and make sure, but hopefully this will help.

--- api/middleware/authenticate.js
+++ api/middleware/authenticate.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 function authenticate(req, res, next) {
-  try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.decoded = decoded;
-    next();
-  } catch (err) {
-    res.status(401).json({ error: 'Unauthorized' });
-  }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(401).json({ error: 'Unauthorized' });
+    } else {
+      req.decoded = decoded;
+      next();
+    }
+  });
 }
 
 module.exports = authenticate;
