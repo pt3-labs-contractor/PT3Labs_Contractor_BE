efb36ef877cae18f320135b0ed7da4e0e0b26292
edreeseg <edward.reeseg@gmail.com>
2019-06-05 10:41:05 -0400

    Completing update to appointment endpoints, created endpoint to retrieve appointments by contractorId

--- api/routes/appointmentRoutes.js
+++ api/routes/appointmentRoutes.js
@@ -51,23 +51,55 @@ router.get('/contractors/:id', async (req, res) => {
   }
 });
 
-// User, contractor, or both allowed to add/edit/delete?
+// User, contractor, or both allowed to add/edit/delete?  Likely need permission from both.  For now, build either.
 
 router.post('/', async (req, res) => {
   try {
+    const { contractorId, serviceId, scheduleId, appointmentDatetime, duration } = req.body;
     const userAppt = await query(
-      'INSERT INTO appointments ("contractorId", "userId", "serviceId", "appointmentDatetime", duration) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      `INSERT INTO appointments ("contractorId", "userId", "serviceId", "scheduleId", "appointmentDatetime", duration) 
+      VALUES ($1, $2, $3, $4, $5, $6) 
+      RETURNING *`,
       [
-        req.body.contractorId,
+        contractorId,
         req.user.id,
-        req.body.serviceId,
-        req.body.apointmentDatetime,
-        req.body.duration,
+        serviceId,
+        scheduleId,
+        appointmentDatetime,
+        duration,
       ]
     );
-    return res.json(userAppt.rows[0]);
+    return res.json({ created: userAppt.rows[0] });
   } catch (err) {
-    return err;
+    return res.status(500).json({ error: 'There was an error while creating appointment.' });
+  }
+});
+
+router.post('/contractor', async (req, res) => {
+  try {
+    if (!req.user.contractorId) throw new Error(403);
+    const { userId, serviceId, scheduleId, appointmentDatetime, duration } = req.body;
+    const userAppt = await query(
+      `INSERT INTO appointments ("contractorId", "userId", "serviceId", "scheduleId", "appointmentDatetime", duration) 
+      VALUES ($1, $2, $3, $4, $5, $6) 
+      RETURNING *`,
+      [
+        req.user.contractorId,
+        userId,
+        serviceId,
+        scheduleId,
+        appointmentDatetime,
+        duration,
+      ]
+    );
+    return res.status(201).json({ created: userAppt.rows[0] });
+  } catch(err) {
+    switch(err.message){
+      case '403':
+        return res.status(403).json({ error: 'Forbidden' });
+      default:
+        return res.status(500).json({ error: 'There was an error while creating appointment.' });
+    }
   }
 });
 
@@ -76,31 +108,51 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   try {
+    const { id } = req.params;
+    const { user } = req;
+    const { appointmentDatetime, duration } = req.body;
+    const appt = await query('SELECT * FROM appointments WHERE id = $1', [id]);
+    if (!appt.rows || !appt.rows[0]) throw new Error(404);
+    if (appt.rows[0].userId !== user.id
+      && appt.rows[0].contractorId !== user.contractorId) throw new Error(403);
     const userAppt = await query(
-      'UPDATE appointments SET "contractorId" = ($1), "appointmentDatetime" = ($2), duration = ($3) WHERE id = ($4) RETURNING *',
-      [
-        req.body.contractorId,
-        req.body.appointmentDatetime,
-        req.body.duration,
-        req.params.id,
-      ]
+      'UPDATE appointments SET "appointmentDatetime" = $1, duration = $2 WHERE id = $3 RETURNING *',
+      [appointmentDatetime, duration, id]
     );
-    return res.json(userAppt.rows[0]);
+    return res.json({ updated: userAppt.rows[0] });
   } catch (err) {
-    return err;
+    switch(err.message){
+      case '403':
+        return res.status(403).json({ error: 'Forbidden' });
+      case '404':
+        return res.status(404).json({ error: 'No appointment with that ID found.' });
+      default:
+        return res.status(500).json({ error: 'There was an error while updating appointment.' });
+    }
   }
 });
 
 // As a callback
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
   const { id } = req.params;
-
-  query('DELETE FROM appointments WHERE id = $1', [id], err => {
-    if (err) {
-      throw err;
+  const { user } = req;
+  const appt = await query('SELECT * FROM appointments WHERE id = $1', [id]);
+  if (!appt.rows || !appt.rows[0]) throw new Error(404);
+  if (appt.rows[0].userId !== user.id
+    && appt.rows[0].contractorId !== user.contractorId) throw new Error(403);
+  await query('DELETE FROM appointments WHERE id = $1', [id]);
+  return res.json({ deleted: appt.rows[0] });
+  } catch(err) {
+    switch(err.message){
+      case '403':
+        return res.status(403).json({ error: 'Forbidden' });
+      case '404':
+        return res.status(404).json({ error: 'No appointment with that ID found.'});
+      default:
+        return res.status(500).json({ error: 'There was an error while deleting appointment.' });
     }
-    res.status(200).send(`appointments deleted with id: ${id}`);
-  });
+  }
 });
 
 module.exports = router;
