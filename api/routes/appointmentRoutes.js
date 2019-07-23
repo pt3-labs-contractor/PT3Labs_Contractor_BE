0ca26b4fdbe40cade30d0621491816e40e16fba8
echopecho <pbsmithtbn@gmail.com>
2019-07-22 21:26:15 -0500

    Fix typo in the /api/appointments get request

--- api/routes/appointmentRoutes.js
+++ api/routes/appointmentRoutes.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
     const attribute = user.contractorId ? 'contractorId' : 'userId';
     const value = isContractor || user.id;
     const appointments = await query(
-      `SELECT a.id, username, c.name as "contractorName", a."contractorId", a."userId", a."serviceId" s.name as service, price, a."startTime", duration, confirmed, a."createdAt" 
+      `SELECT a.id, username, c.name as "contractorName", a."contractorId", a."userId", a."serviceId", s.name as service, price, a."startTime", duration, confirmed, a."createdAt" 
       FROM appointments a 
       JOIN contractors c
       ON c.id = a."contractorId"
